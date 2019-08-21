c818fed7099bd8b2030f376a89e54d6b9693aa2a
Corina Tudorache <corina_ww@yahoo.com>
2019-08-21 20:44:05 +0100

    Update bootstrap.sh
    updated bootstrap.sh

--- foss4g-2019/bootstrap.sh
+++ foss4g-2019/bootstrap.sh
@@ -12,9 +12,9 @@ echo "---Install QGIS 3.8 with GRASS and SAGA---"
 sudo sh -c 'echo "deb https://qgis.org/ubuntugis bionic main" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb-src https://qgis.org/ubuntugis bionic main" >> /etc/apt/sources.list'
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
-wget -O - https://qgis.org/downloads/qgis-2017.gpg.key | gpg --import
-gpg --fingerprint CAEB3DC3BDF7FB45
-gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
+wget -O - https://qgis.org/downloads/qgis-2019.gpg.key | gpg --import
+gpg --fingerprint 51F523511C7028C3
+gpg --export --armor gpg --fingerprint 51F523511C7028C3 | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install qgis python-qgis qgis-plugin-grass saga -y
 
