22da3ac61f29d7169ab8b605be60fc2ee8f3bc55
Corina Tudorache <corina_ww@yahoo.com>
2019-08-28 16:20:13 +0300

    Update bootstrap.sh

--- foss4g-2019/bootstrap.sh
+++ foss4g-2019/bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-#Install Graphical User Interface Lubuntu Desktop
+#Install Graphical User Interface - Lubuntu Desktop
 echo "---Install Lubuntu Desktop---"
 sudo apt install --no-install-recommends lubuntu-desktop -y
 sudo apt-get install virtualbox-guest-x11 virtualbox-guest-utils virtualbox-guest-dkms -y
@@ -12,7 +12,7 @@ echo "Install Java"
 sudo apt install default-jre -y
 sudo apt install openjdk-8-jre-headless
 
-#install qgis 3.8
+#Install QGIS 3.8
 echo "---Install QGIS 3.8 with GRASS and SAGA---"
 sudo sh -c 'echo "deb https://qgis.org/ubuntugis bionic main" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb-src https://qgis.org/ubuntugis bionic main" >> /etc/apt/sources.list'
@@ -23,19 +23,19 @@ gpg --export --armor gpg --fingerprint 51F523511C7028C3 | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install qgis python-qgis qgis-plugin-grass saga -y
 
-#Install PostgreSQL-11 with PosGIS-2.5.2
+#Install PostgreSQL-11 with PostGIS-2.5.2
 echo "---Install PostgreSQL-11 with PosGIS-2.5.2---"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo wget --no-check-certificate --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
-sudo apt install -y postgresql-11 postgresql-11-postgis-2.5 postgis-2.5-utils postgis-2.5-client postgresql-11-postgis-2.5-scripts postgresql-contrib-11 postgresql-client-11
+sudo apt install -y postgresql-11 postgresql-11-postgis-2.5 postgresql-11-postgis-2.5-scripts postgresql-11 postgresql-client-11
 sudo apt install postgis -y
 
-#add postgis and postgis topology extention to postgres database
+#Add postgis and postgis topology extentions to postgres database
 echo "---Add postgis and postgis topology---"
 sudo -u postgres psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" postgres
 
-# creating password for user postgres
+#Create password for user postgres
 echo "---Create password for user postgres---"
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 sudo systemctl restart postgresql.service
