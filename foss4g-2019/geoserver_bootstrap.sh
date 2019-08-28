2b282aecd27d3b153da862a0af752c4e07f8c440
Corina Tudorache <corina_ww@yahoo.com>
2019-08-28 07:17:33 +0300

    Update geoserver_bootstrap.sh

--- foss4g-2019/geoserver_bootstrap.sh
+++ foss4g-2019/geoserver_bootstrap.sh
@@ -2,8 +2,8 @@
 
 #Install GeoServer-2.15.1
 echo "Download and install GeoServer-2.15.1"
-wget -N 'http://sourceforge.net/projects/geoserver/files/GeoServer/2.15.1/geoserver-2.15.1-bin.zip'
-unzip geoserver-2.15.1-bin.zip
+wget -nc 'http://sourceforge.net/projects/geoserver/files/GeoServer/2.15.1/geoserver-2.15.1-bin.zip' --output-document=geoserver-2.15.1.zip
+unzip geoserver-2.15.1.zip
 sudo mv geoserver-2.15.1 /usr/share/geoserver
 sudo echo "export GEOSERVER_HOME=/usr/share/geoserver" >> ~/.profile
 source ~/.profile
